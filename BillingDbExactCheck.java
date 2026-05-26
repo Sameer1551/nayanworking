ee2c8664f2f73c0cc5c78849f5b714fdf21a6fed
Sameer <siddheshvikram1@gmail.com>
2026-05-26 18:11:40 +0530

    Progress update (2/5)

--- BillingDbExactCheck.java
+++ BillingDbExactCheck.java
@@ -2,3 +2,7 @@ import java.sql.*;
 public class BillingDbExactCheck {
   public static void main(String[] args) throws Exception {
     String url = "jdbc:mysql://localhost:3306/nayan-db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    try (Connection conn = DriverManager.getConnection(url, "root", "root")) {
+      try (Statement st = conn.createStatement()) {
+        try (ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM billing_records")) { rs.next(); System.out.println("billing_records exact=" + rs.getInt(1)); }
+        try (ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM billing_products")) { rs.next(); System.out.println("billing_products exact=" + rs.getInt(1)); }
