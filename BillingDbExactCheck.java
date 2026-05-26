174a66d62ba1476f9ce602395b25e9b3b3d0a81c
Sameer <siddheshvikram1@gmail.com>
2026-05-26 22:30:42 +0530

    Add functionality (3/5)

--- BillingDbExactCheck.java
+++ BillingDbExactCheck.java
@@ -6,3 +6,7 @@ public class BillingDbExactCheck {
       try (Statement st = conn.createStatement()) {
         try (ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM billing_records")) { rs.next(); System.out.println("billing_records exact=" + rs.getInt(1)); }
         try (ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM billing_products")) { rs.next(); System.out.println("billing_products exact=" + rs.getInt(1)); }
+        try (ResultSet rs = st.executeQuery("SELECT id, bill_number FROM billing_records ORDER BY id")) {
+          while (rs.next()) {
+            System.out.println("id=" + rs.getLong(1) + ", bill=" + rs.getString(2));
+          }
