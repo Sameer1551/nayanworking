1deb61feb90a2bf61e5e08ac9ac2fc001dbb8b5b
Sameer <siddheshvikram1@gmail.com>
2026-05-26 17:58:46 +0530

    Cleanup code (2/5)

--- backup_db.bat
+++ backup_db.bat
@@ -9,3 +9,14 @@ setlocal
 set DB_USER=root
 set DB_PASS=root
 set DB_NAME=nayan-db
+set BACKUP_DIR=D:\nayan\databackuptest
+set MYSQLDUMP_PATH="C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe"
+:: PHASE 8: Backup Encryption Password
+set BACKUP_ENCRYPTION_PASS=SuperSecretBackupKey2026!
+
+:: Create backup directory if it doesn't exist
+if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
+
+:: Generate timestamp
+for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+for /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
